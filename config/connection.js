e11040e22a95ee6e7eb1ef4ec8559d84a0c81727
Nathan Stepper <nstepper@uw.edu>
2023-06-23 23:30:35 -0700

    try to fix the server

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
+      // Set debug option to true
+      mongoose.set('debug', true);
       await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cowboys-in-a-ranch', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
